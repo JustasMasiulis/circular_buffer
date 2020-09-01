d0c2f6ff9b3a0b4776326d36ec7a2914d01b7603
ueberaccelerate <ueberaccelerate@gmail.com>
2020-09-01 12:34:57 +0300

    * cmake update:   -- add get_cpm instead of CPM.cmake

--- cmake/Dependency.cmake
+++ cmake/Dependency.cmake
@@ -1,23 +1,27 @@
-include ( cmake/CPM.cmake )
+include ( cmake/get_cpm.cmake )
 
-CPMAddPackage(
-  NAME Catch2
-  GITHUB_REPOSITORY catchorg/Catch2
-  VERSION 2.5.0
-)
+function (add_catch_and_benchmark)
+	CPMAddPackage(
+	  NAME Catch2
+	  GITHUB_REPOSITORY catchorg/Catch2
+	  VERSION 2.5.0
+	)
+	
+	CPMAddPackage(
+	  NAME benchmark
+	  GITHUB_REPOSITORY google/benchmark
+	  VERSION 1.4.1
+	  OPTIONS
+		"BENCHMARK_ENABLE_TESTING Off"
+		"BENCHMARK_ENABLE_EXCEPTIONS Off"
+		"BENCHMARK_ENABLE_GTEST_TESTS Off"
+		"BENCHMARK_ENABLE_INSTALL Off"
+	)
 
-CPMAddPackage(
-  NAME benchmark
-  GITHUB_REPOSITORY google/benchmark
-  VERSION 1.4.1
-  OPTIONS
-    "BENCHMARK_ENABLE_TESTING Off"
-	"BENCHMARK_ENABLE_EXCEPTIONS Off"
-	"BENCHMARK_ENABLE_GTEST_TESTS Off"
-	"BENCHMARK_ENABLE_INSTALL Off"
-)
+	if (benchmark_ADDED)
+	  # compile with C++17
+	  set_target_properties(benchmark PROPERTIES CXX_STANDARD 17)
+	endif()
+endfunction()
 
-if (benchmark_ADDED)
-  # compile with C++17
-  set_target_properties(benchmark PROPERTIES CXX_STANDARD 17)
-endif()
+
